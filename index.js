6bddd17668367aa197da4af63fb165c7ab96ee0a
EGCHAT <egchat@egchat.com>
2026-05-07 19:13:17 +0100

    auto: update backend

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/.well-known/assetlinks.json', (req, res) => {
       namespace: 'android_app',
       package_name: 'com.egchat.app',
       sha256_cert_fingerprints: [
-        process.env.APP_SHA256_FINGERPRINT || 'REEMPLAZAR_CON_SHA256_DE_TU_KEYSTORE'
+        process.env.APP_SHA256_FINGERPRINT || 'FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C'
       ]
     }
   }]);
